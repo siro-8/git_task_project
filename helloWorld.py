e25f5c4315fc1964a7a64bfb5972dee952502cd2
siro-8 <tramitamiroslav@gmail.com>
2024-03-26 20:52:13 +0000

    added basic comments

--- helloWorld.py
+++ helloWorld.py
@@ -1,5 +1,7 @@
-# write simple code taking input from the user and printing it
+# write simple code taking input from the user and printing it 
 
+# take input from user
 user_message = input("Please enter your message to be printed: ")
 
+# print it
 print(user_message)
